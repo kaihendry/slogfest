8606f9e9427dd68b8b1c759f02aa3e1455001d3e
Kai Hendry <hendry@iki.fi>
2023-01-25 17:04:18 +0800

    need middleware

--- main.go
+++ main.go
@@ -13,7 +13,8 @@ import (
 func main() {
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout)))
 
-	http.HandleFunc("/", sleep())
+	http.HandleFunc("/foo", sleep())
+	http.HandleFunc("/bar", basic())
 
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
@@ -30,6 +31,18 @@ func sleep() func(http.ResponseWriter, *http.Request) {
 
 		slog.Info("slept", "sleep", sleep, "duration", time.Since(start))
 
+		slog.Info("finished",
+			slog.Group("req",
+				slog.String("method", r.Method),
+				slog.String("url", r.URL.String())),
+			slog.Int("status", http.StatusOK),
+			slog.Duration("duration", time.Since(start)))
+	}
+}
+
+func basic() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
 		slog.Info("finished",
 			slog.Group("req",
 				slog.String("method", r.Method),
