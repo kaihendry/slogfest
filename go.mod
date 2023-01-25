7ce18d5f79dbfb4d67f4c631a0aebacbb0a9413e
Kai Hendry <hendry@iki.fi>
2023-01-25 13:36:46 +0800

    inital

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kaihendry/slog-test
+
+go 1.19
+
+require golang.org/x/exp v0.0.0-20230124195608-d38c7dcee874 // indirect
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"golang.org/x/exp/slog"
+)
+
+func main() {
+	// log with compact timestamps
+	logger := slog.New(slog.NewTextHandler(os.Stdout))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		sleepQuery := r.URL.Query().Get("sleep")
+		sleep, _ := strconv.Atoi(sleepQuery)
+
+		logger.Info("sleeping", "sleep", sleep)
+		time.Sleep(time.Duration(sleep) * time.Second)
+		fmt.Fprintf(w, "Slept for %d seconds", sleep)
+
+		logger.Info("slept", "sleep", sleep, "duration", time.Since(start))
+
+		slog.Info("finished",
+			slog.Group("req",
+				slog.String("method", r.Method),
+				slog.String("url", r.URL.String())),
+			slog.Int("status", http.StatusOK),
+			slog.Duration("duration", time.Since(start)))
+
+	})
+	http.ListenAndServe(":8080", nil)
+}
