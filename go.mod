e24a9d8b8121217c5f350ef329344005db810a2a
Kai Hendry <hendry@iki.fi>
2023-02-02 16:03:24 +0800

    refactor: simpler example

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/kaihendry/slog-test
 
 go 1.19
 
-require golang.org/x/exp v0.0.0-20230131013936-aae9b4e6329d
+require golang.org/x/exp v0.0.0-20230131160201-f062dba9d201
+
+require github.com/kaihendry/gotrace v0.0.0-20230202074634-1cd79687a985 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"net/http"
 	"os"
-	"time"
 
 	"github.com/kaihendry/gotrace"
 	"golang.org/x/exp/slog"
@@ -11,21 +11,8 @@ import (
 func main() {
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout)))
 
-	// https://github.com/apex/log/blob/master/_examples/trace/trace.go
+	var err error
+	defer gotrace.Trace("fetching", "env", "dev").Stop(&err)
+	_, err = http.Get("https://httpbin.org/delay/2")
 
-	ctx := slog.Group("fields",
-		slog.String("app", "myapp"),
-		slog.String("env", "prod"))
-
-	for range time.Tick(time.Second) {
-		_ = work(ctx)
-	}
-
-}
-
-func work(ctx slog.Attr) (err error) {
-	path := "README.md"
-	defer gotrace.Trace("opening").Stop(&err)
-	_, err = os.Open(path)
-	return
 }
