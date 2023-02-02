ddf5af231ea6ff51bd18ea560510cf95b476d9b8
Kai Hendry <hendry@iki.fi>
2023-02-02 15:46:08 +0800

    refactor: simple example

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/kaihendry/gotrace"
 	"golang.org/x/exp/slog"
 )
 
@@ -23,29 +24,8 @@ func main() {
 }
 
 func work(ctx slog.Attr) (err error) {
-	path := "/mnt/redsamba/freenas.mp4"
-	defer Trace("opening").Stop(&err)
+	path := "README.md"
+	defer gotrace.Trace("opening").Stop(&err)
 	_, err = os.Open(path)
 	return
-}
-
-// Just want to entend slog.Record with a Stop function
-type traceEntry struct {
-	r     slog.Record
-	start time.Time
-}
-
-func Trace(msg string, kvs ...any) (v traceEntry) {
-	slog.Info(msg, kvs...)
-	v.r.Message = msg
-	v.start = time.Now()
-	return v
-}
-
-func (v traceEntry) Stop(err *error) {
-	if err == nil || *err == nil {
-		slog.Info(v.r.Message, "duration", time.Since(v.start).Milliseconds())
-	} else {
-		slog.Error(v.r.Message, *err, "duration", time.Since(v.start).Milliseconds())
-	}
 }
